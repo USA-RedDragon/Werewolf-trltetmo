b532f6508147bfb3715f55e0cd64b92e7885eaf9
Jacob McSwain <jacob.a.mcswain@gmail.com>
2016-08-09 06:47:49 -0500

    Add build stuff, zip skeleton, and branding

---
+++ build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+export kernel=Werewolf
+export device=trltetmo
+export deviceconfig=werewolf_defconfig
+export outdir=/usr/share/nginx/html/Werewolf
+export makeopts="-j$(nproc)"
+export zImagePath="arch/arm/boot/zImage"
+export KBUILD_BUILD_USER=USA-RedDragon
+export KBUILD_BUILD_HOST=EdgeOfCreation
+export CROSS_COMPILE="ccache /root/krexus/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-"
+export ARCH=arm
+
+export version=$(cat version)
+
+if [[ $1 =~ "clean" ]] ; then
+	make clean
+	make mrproper
+fi
+
+make ${makeopts} ${deviceconfig}
+make ${makeopts}
+
+./dtbTool -o zip/dtb -s 4096 -p scripts/dtc/ arch/arm/boot/
+
+if [ -a ${zImagePath} ] ; then
+	cp ${zImagePath} zip/zImage
+	find -name '*.ko' -exec cp -av {} zip//modules/ \;
+	cd zip
+	zip -q -r ${kernel}-${device}-${version}.zip anykernel.sh  META-INF tools zImage dtb
+else
+	echo -e "\n\e[31m***** Build Failed *****\e[0m\n"
+fi
+
+if ! [ -d ${outdir} ] ; then
+	mkdir ${outdir}
+fi
+
+if [ -a ${kernel}-${device}-${version}.zip ] ; then
+	mv -v ${kernel}-${device}-${version}.zip ${outdir}
+fi
+
+rm -f zip/zImage
+rm -f zip/dtb
